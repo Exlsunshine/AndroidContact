07fdca76d6ff4018a04bdfde475083911061cd86
sunshine <sunshine46582747@sina.com>
2015-01-09 14:07:36 +0800

    Add bulk delete operation in MainActivity page.

--- src/com/example/activity/MainActivity.java
+++ src/com/example/activity/MainActivity.java
@@ -13,21 +13,26 @@ import com.zijunlin.Zxing.Demo.CaptureActivity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.MenuItem.OnActionExpandListener;
 import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.SearchView.OnQueryTextListener;
 
 public class MainActivity extends Activity
@@ -38,6 +43,15 @@ public class MainActivity extends Activity
 	
 	private ArrayList<Contact> contactList;
 	private ContactListViewAdapter contactAdapter;
+	
+	// 2015-1-9
+	private ArrayList<String> deleteList;
+	private static int DELETE_MODE = 1;
+	private static int SELECT_MODE = 2;
+	private static int MODE = SELECT_MODE;
+	private TextView delete;
+	private TextView all;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -71,6 +85,10 @@ public class MainActivity extends Activity
 		
 		contactAdapter = new ContactListViewAdapter(this,contactList);
 		contactListview.setAdapter(contactAdapter);
+		
+		// 2015-1-9
+		deleteList = new ArrayList<String>();
+		contactListview.setBackgroundColor(Color.WHITE);
 	}
 	
 	private void initUISettings()
@@ -84,16 +102,114 @@ public class MainActivity extends Activity
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) 
 			{
-				Intent it = new Intent(MainActivity.this, ContactDetailsActivity.class);
-				it.putExtra(DatabaseHandler.KEY_ID, ((Integer)contactList.get(arg2).getID()).intValue());
-				
-				MainActivity.this.startActivity(it);
-				
-				int version = Integer.valueOf(android.os.Build.VERSION.SDK);  
-				if(version >= 5)
-					overridePendingTransition(R.anim.close_enter, R.anim.close_exit);
+				if (MODE == SELECT_MODE)
+				{
+					Intent it = new Intent(MainActivity.this, ContactDetailsActivity.class);
+					it.putExtra(DatabaseHandler.KEY_ID, ((Integer)contactList.get(arg2).getID()).intValue());
+					
+					MainActivity.this.startActivity(it);
+					
+					int version = Integer.valueOf(android.os.Build.VERSION.SDK);  
+					if(version >= 5)
+						overridePendingTransition(R.anim.close_enter, R.anim.close_exit);
+				}
+				else if (MODE == DELETE_MODE)
+				{
+					if (deleteList.contains(String.valueOf(arg2)))
+					{
+						deleteList.remove(String.valueOf(arg2));
+						arg1.setBackgroundColor(Color.WHITE);
+					}
+					else
+					{
+						deleteList.add(String.valueOf(arg2));
+						arg1.setBackgroundColor(Color.LTGRAY);
+					}
+				}
 			}
 		});
+		
+		// 2015-1-9
+		delete = (TextView)findViewById(R.id.delete);
+		delete.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View arg0)
+			{
+				if (deleteList.isEmpty())
+				{
+					Toast.makeText(MainActivity.this, "Please select items.", Toast.LENGTH_SHORT).show();
+					return ;
+				}
+				AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+				builder.setTitle("Are you sure to delete contact?");
+				builder.setMessage("Click Confirm to delete!");
+				builder.setCancelable(true);
+				builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() 
+				{
+					@Override
+					public void onClick(DialogInterface arg0, int arg1) 
+					{
+						if (!deleteList.isEmpty() && MODE == DELETE_MODE)
+						{
+							DatabaseHandler db = new DatabaseHandler(MainActivity.this);
+							
+							for (int i = 0; i < deleteList.size(); i++)
+							{
+								Contact contact = contactList.get(Integer.parseInt(deleteList.get(i)));
+								db.deleteContact(contact);
+							}
+							
+							Toast.makeText(MainActivity.this, "Delete successful!", Toast.LENGTH_SHORT).show();
+							deleteList.clear();
+							onResume();
+						}
+					}
+				});
+				builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() 
+				{
+					@Override
+					public void onClick(DialogInterface arg0, int arg1)
+					{
+						arg0.cancel();
+					}
+				});
+				AlertDialog dialog = builder.create();
+				dialog.show();
+			}
+		});
+
+		all = (TextView)findViewById(R.id.all);
+		all.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View arg0)
+			{
+				if (MODE == DELETE_MODE)
+				{
+					if (deleteList.size() != contactListview.getChildCount())
+					{
+						deleteList.clear();
+						for (int i = 0; i < contactListview.getChildCount(); i++)
+						{
+							View view = contactListview.getChildAt(i);
+							view.setBackgroundColor(Color.LTGRAY);
+							deleteList.add(String.valueOf(i));
+						}
+					}
+					else
+					{
+						deleteList.clear();
+						for (int i = 0; i < contactListview.getChildCount(); i++)
+						{
+							View view = contactListview.getChildAt(i);
+							view.setBackgroundColor(Color.WHITE);
+						}
+					}
+				}
+			}
+		});
+		
 	}
 	
 	private class LetterListViewListener implements  com.example.implementation.SlideBar.OnTouchingLetterChangedListener  
@@ -139,6 +255,43 @@ public class MainActivity extends Activity
 			{
 				Intent scanIntent = new Intent(MainActivity.this, CaptureActivity.class);
 				startActivity(scanIntent);
+				return true;
+			}
+		});
+	    
+	    
+	    MenuItem mode = menu.findItem(R.id.action_delete_mode);
+	    mode.setOnMenuItemClickListener(new OnMenuItemClickListener() 
+	    {
+			@Override
+			public boolean onMenuItemClick(MenuItem arg0) 
+			{
+				if (MODE == SELECT_MODE)
+				{
+					delete.setVisibility(View.VISIBLE);
+					delete.setClickable(true);
+					all.setVisibility(View.VISIBLE);
+					all.setClickable(true);
+					MODE = DELETE_MODE;
+				}
+				else if (MODE == DELETE_MODE)
+				{
+					delete.setVisibility(View.GONE);
+					delete.setClickable(false);
+					all.setVisibility(View.GONE);
+					all.setClickable(false);
+					MODE = SELECT_MODE;
+					
+					for (int i = 0; i < contactListview.getChildCount(); i++)
+					{
+						View view = contactListview.getChildAt(i);
+						view.setBackgroundColor(Color.WHITE);
+					}
+					deleteList.clear();
+					
+					//onRestart();
+				}
+				
 				return true;
 			}
 		});
